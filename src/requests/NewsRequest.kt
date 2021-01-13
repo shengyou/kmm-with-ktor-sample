520a8b173d18a03fc99a7cbf89727d8d29f53022
Shengyou Fan <shengyoufan@gmail.com>
2021-01-13 16:22:00 +0800

    turn on ignore properties

--- src/requests/NewsRequest.kt
+++ src/requests/NewsRequest.kt
@@ -1,5 +1,8 @@
 package io.kraftsman.requests
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class NewsRequest(
     val id: Int,
     val title: String,
