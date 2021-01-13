56e07d82e862d4e64facde985fdc8d36af3adfee
Shengyou Fan <shengyoufan@gmail.com>
2021-01-13 13:56:12 +0800

    add h2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
+val h2_version: String by project
 
 plugins {
     application
@@ -31,6 +32,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
